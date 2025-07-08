eb0e861eb7f2803026626806a32c147064d9a003
Cesar Eduardo Chahuas Rebatta <cesar.chahuasrebatta@gmail.com>
2025-07-08 01:14:58 -0500

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ def extended_gcd(a, b):
         return a, 1, 0
     gcd, x1, y1 = extended_gcd(b, a % b)
     x = y1
-n    y = x1 - (a // b) * y1
+    y = x1 - (a // b) * y1
     return gcd, x, y
 
 # Función para resolver ax + by = c
